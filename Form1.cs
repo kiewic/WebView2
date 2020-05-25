60a92e18e575429d3c8f316b644240ccd0a5f557
Gilberto Stankiewicz <874134+kiewic@users.noreply.github.com>
2020-05-25 13:33:57 -0700

    Add project files

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.WinForms;
+
+namespace WebView2App
+{
+    public partial class Form1 : Form
+    {
+        WebView2 webView;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            webView = new WebView2();
+            webView.Dock = DockStyle.Fill;
+            webView.Source = new Uri("https://example.com");
+            this.Controls.Add(webView);
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await Task.Delay(7000);
+            webView.CoreWebView2.Navigate("https://kiewic.com");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# WebView2 WinForm application sample
+
+Documentation is [here](https://docs.microsoft.com/en-us/microsoft-edge/webview2/gettingstarted/winforms).
